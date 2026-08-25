8117fee307f7fbee4677415f9989a08f6a6c4936
DanielDiasG <daniel.dias.g08@gmail.com>
2026-08-25 09:09:37 -0300

    new file:   Agenda4.css 	modified:   Agenda4.php

---
+++ Agenda4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Tarefas</title>
+<!-- link do css externo -->
+<link rel="stylesheet" href="Agenda4.css">
+</head>
+<body>
+    
+</body>
+</html>
